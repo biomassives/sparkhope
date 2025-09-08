76273745600c80e97107c10c6a88ee5dbf587de0
G. Willson <ecocommunity@protonmail.com>
2025-09-08 08:54:01 -0600

    strange 404 on the web version!

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,7 @@
 
-import __ASTRO_IMAGE_IMPORT_Z1TMPA9 from "../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
 import __ASTRO_IMAGE_IMPORT_Z1zJUXx from "../../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_Z1TMPA9 from "../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
 import __ASTRO_IMAGE_IMPORT_29VmAE from "../../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fsw%2Findex.mdx";
 import __ASTRO_IMAGE_IMPORT_2h1pVV from "../../../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Fguide%2Findex.md";
-export default new Map([["../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1TMPA9], ["../../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1zJUXx], ["../../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fsw%2Findex.mdx", __ASTRO_IMAGE_IMPORT_29VmAE], ["../../../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Fguide%2Findex.md", __ASTRO_IMAGE_IMPORT_2h1pVV]]);
+export default new Map([["../../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1zJUXx], ["../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1TMPA9], ["../../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fsw%2Findex.mdx", __ASTRO_IMAGE_IMPORT_29VmAE], ["../../../../assets/hero.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fen%2Fguide%2Findex.md", __ASTRO_IMAGE_IMPORT_2h1pVV]]);
 		
